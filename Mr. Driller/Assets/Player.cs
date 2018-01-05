c27280b5732c6942287ec3f134fdd8015170ef3f
anson-li <ansonli.94@gmail.com>
2018-01-04 18:00:30 -0700

    Added demo game, sample build, and base file structure

---
+++ Mr. Driller/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float movement = 0f;
+
+	public float speed = 10f;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		movement = Input.GetAxis("Horizontal") * speed;
+	}
+
+	void FixedUpdate() {
+		Vector2 velocity = rb.velocity;
+		velocity.x = movement;
+		rb.velocity = velocity;
+	}
+}
