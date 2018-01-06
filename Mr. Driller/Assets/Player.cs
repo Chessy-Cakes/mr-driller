a94cd63bed39772f84b45dd16092cd37737a3cc8
Colin Tong <tongcolin@live.ca>
2018-01-06 04:43:19 -0500

    Fixed syntax errors from merge

--- Mr. Driller/Assets/Player.cs
+++ Mr. Driller/Assets/Player.cs
@@ -32,53 +32,53 @@ public class Player : MonoBehaviour
 			movement = 0;
 		}
 
-		// Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
-		#if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+        // Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
+        #if UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 
-		// Check if Input has registered more than zero touches
-		if (Input.touchCount > 0) {
-			// Store the first touch detected.
-			Touch myTouch = Input.touches [0];
+		    // Check if Input has registered more than zero touches
+		    if (Input.touchCount > 0) {
+			    // Store the first touch detected.
+			    Touch myTouch = Input.touches [0];
 
-			// Check if the phase of that touch equals Began
-			if (myTouch.phase == TouchPhase.Began) {
-				// If so, set touchOrigin to the position of that touch
-				touchOrigin = myTouch.position;
-			}
+			    // Check if the phase of that touch equals Began
+			    if (myTouch.phase == TouchPhase.Began) {
+				    // If so, set touchOrigin to the position of that touch
+				    touchOrigin = myTouch.position;
+			    }
 
-		// If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-		else if ((myTouch.phase == TouchPhase.Stationary || myTouch.phase == TouchPhase.Moved) && touchOrigin.x >= 0) {
-				// Set touchEnd to equal the position of this touch
-				Vector2 touchEnd = myTouch.position;
+		    // If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+                else if ((myTouch.phase == TouchPhase.Stationary || myTouch.phase == TouchPhase.Moved) && touchOrigin.x >= 0) {
+				    // Set touchEnd to equal the position of this touch
+				    Vector2 touchEnd = myTouch.position;
 
-				// Calculate the difference between the beginning and end of the touch on the x axis.
-				float x = touchEnd.x - touchOrigin.x;
+				    // Calculate the difference between the beginning and end of the touch on the x axis.
+				    float x = touchEnd.x - touchOrigin.x;
 
-				// Calculate the difference between the beginning and end of the touch on the y axis.
-				float y = touchEnd.y - touchOrigin.y;
+				    // Calculate the difference between the beginning and end of the touch on the y axis.
+				    float y = touchEnd.y - touchOrigin.y;
 
-				// Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
-				// touchOrigin.x = -1;
+				    // Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+				    // touchOrigin.x = -1;
 
-				// Check if the difference along the x axis is greater than the difference along the y axis.
-				if (Mathf.Abs (x) > Mathf.Abs (y)) {
-					// TODO: Add functionaity to add AttemptMove to include drill functionality
-					// If x is greater than zero, set horizontal to speed, otherwise set it to -speed
-					movement = x > 0 ? speed : -speed;
-				} else {
-					// TODO: Add functionality to vertical to include sliding up 1 block vs. drilling
-					// If y is greater than zero, set horizontal to 1, otherwise set it to -1
-					// vertical = y > 0 ? 1 : -1;
-				}
-			} else {
-				movement = 0;
-			}
+				    // Check if the difference along the x axis is greater than the difference along the y axis.
+				    if (Mathf.Abs (x) > Mathf.Abs (y)) {
+					    // TODO: Add functionaity to add AttemptMove to include drill functionality
+					    // If x is greater than zero, set horizontal to speed, otherwise set it to -speed
+					    movement = x > 0 ? speed : -speed;
+				    } else {
+					    // TODO: Add functionality to vertical to include sliding up 1 block vs. drilling
+					    // If y is greater than zero, set horizontal to 1, otherwise set it to -1
+					    // vertical = y > 0 ? 1 : -1;
+				    }
+			    } else {
+				    movement = 0;
+			    }
+		    }
 
-			#endif //End of mobile platform dependendent compilation section started above with #elif
-		}
-	}
+        #endif //End of mobile platform dependendent compilation section started above with #elif
+    }
 
-	void FixedUpdate ()
+    void FixedUpdate ()
 	{
 		Vector2 velocity = rb.velocity;
 		velocity.x = movement;
