7e801a2e62e335101b59046607d4cc5fef9f33ee
anson-li <ansonli.94@gmail.com>
2018-01-07 17:58:13 -0700

    Added object inheritance for moving objects, added a restart button, readded base drill mechanic from master

--- Mr. Driller/Assets/Player.cs
+++ Mr. Driller/Assets/Player.cs
@@ -2,34 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MovingObject
 {
 
 	// Used to store location of screen touch origin for mobile controls.
 	private Vector2 touchOrigin = -Vector2.one;
-
 	Rigidbody2D rb;
-	float movement = 0f;
-	public float speed = 10f;
+	private int horizontal = 0;
+	private int vertical = 0;
 
-	void Start ()
+	public int speed = 1;
+
+	protected override void Start () 
 	{
 		rb = GetComponent<Rigidbody2D> ();
+		base.Start ();
 	}
 
-	void Update ()
+	protected void Update ()
 	{
+
+		horizontal = 0;     //Used to store the horizontal move direction.
+		vertical = 0;       //Used to store the vertical move direction.
 
 		// Check if we are running either in the Unity editor or in a standalone build.
 		// #if UNITY_STANDALONE || UNITY_WEBPLAYER
 
 		// Standard keyboard input 
 		if (Input.GetKey ("right")) {
-			movement = speed;
+			horizontal = speed;
 		} else if (Input.GetKey ("left")) {
-			movement = -speed;
+			horizontal = -speed;
 		} else {
-			movement = 0;
+			horizontal = 0;
+		}
+
+		if (Input.GetKey ("up")) {
+			vertical = 0; //speed
+		} else if (Input.GetKey ("down")) {
+			vertical = -speed;
+		} else {
+			vertical = 0;
 		}
 
 		// Check if we are running on iOS, Android, Windows Phone 8 or Unity iPhone
@@ -45,9 +58,8 @@ public class Player : MonoBehaviour
 				// If so, set touchOrigin to the position of that touch
 				touchOrigin = myTouch.position;
 			}
-
-		// If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
-		else if ((myTouch.phase == TouchPhase.Stationary || myTouch.phase == TouchPhase.Moved) && touchOrigin.x >= 0) {
+			// If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+			else if ((myTouch.phase == TouchPhase.Stationary || myTouch.phase == TouchPhase.Moved) && touchOrigin.x >= 0) {
 				// Set touchEnd to equal the position of this touch
 				Vector2 touchEnd = myTouch.position;
 
@@ -64,24 +76,43 @@ public class Player : MonoBehaviour
 				if (Mathf.Abs (x) > Mathf.Abs (y)) {
 					// TODO: Add functionaity to add AttemptMove to include drill functionality
 					// If x is greater than zero, set horizontal to speed, otherwise set it to -speed
-					movement = x > 0 ? speed : -speed;
+					horizontal = x > 0 ? speed : -speed;
 				} else {
 					// TODO: Add functionality to vertical to include sliding up 1 block vs. drilling
 					// If y is greater than zero, set horizontal to 1, otherwise set it to -1
-					// vertical = y > 0 ? 1 : -1;
+					vertical = y > 0 ? 0 : -speed;
 				}
 			} else {
-				movement = 0;
+				// horizontal = 0;
+				// vertical = 0;
 			}
+		}
+		#endif
+			
+		if (horizontal != 0) { // || vertical != 0
+			// AttemptMove<Component> (horizontal, vertical);
+			Vector2 velocity = rb.velocity;
+			velocity.x = horizontal;
+			rb.velocity = velocity;
+		}
 
-			#endif //End of mobile platform dependendent compilation section started above with #elif
+		if (vertical != 0) {
+			AttemptMove<Component> (horizontal, vertical);
 		}
 	}
-
-	void FixedUpdate ()
+		
+	protected override void AttemptMove <T> (int xDir, int yDir)
 	{
-		Vector2 velocity = rb.velocity;
-		velocity.x = movement;
-		rb.velocity = velocity;
+		base.AttemptMove <T> (xDir, yDir);
+		// Add actions here to be made after the move attempt was made
+		RaycastHit2D hit;
 	}
+		
+	protected override void OnCantMove <T> (T component)
+	{
+		// Set hitWall to equal the component passed in as a parameter.
+		GameObject hitBlock = component as GameObject;
+		GameObject.Destroy (component.gameObject);
+	}
+		
 }
